6690289247b6ca155cf5c75740761a1999bf3259
Luiz Fernando Duarte Patriani <duartepatriani@gmail.com>
2020-09-30 17:28:40 -0300

    feat add defition file

---
+++ www/libVLCPlayer.d.ts
@@ -0,0 +1,51 @@
+interface IlibVLCPlayer {
+	play(
+		url: string,
+		options?: IlibVLCPlayerOptions,
+		success?: () => void,
+		failed?: () => void,
+	): void;
+	playNext(
+		url: string,
+		options: IlibVLCPlayerBaseOptions,
+		success?: () => void,
+		failed?: () => void,
+	): void;
+	stop(success?: () => void, failed?: () => void): void;
+	pause(success?: () => void, failed?: () => void): void;
+	close(success?: () => void, failed?: () => void): void;
+
+	// returns: {position, current_location (00:00), duration (00:00)}
+	getPosition(
+		success?: (position: number, current_location: string, duration: string) => void,
+		failed?: () => void,
+	): void;
+
+	// position must be 1 to 100 only
+	seekPosition(position: number, success?: () => void, failed?: () => void): void;
+
+	changePositionAndSize(
+		options: IlibVLCPlayerPositionAndSize,
+		success?: () => void,
+		failed?: () => void,
+	);
+}
+
+interface IlibVLCPlayerBaseOptions {
+	autoPlay?: bool;
+	hideControls?: bool;
+}
+
+interface IlibVLCPlayerOptions extends IlibVLCPlayerBaseOptions, IlibVLCPlayerPositionAndSize {}
+
+interface IlibVLCPlayerPositionAndSize {
+	fullscreen?: bool;
+	top?: number;
+	left?: number;
+	width?: number;
+	height?: number;
+}
+
+interface Window {
+	libVLCPlayer: IlibVLCPlayer;
+}
